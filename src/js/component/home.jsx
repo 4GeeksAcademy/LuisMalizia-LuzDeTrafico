3ccd1bf79b59fbc076ee474f84d3258309bdc5a0
Luis Malizia <luisfmaliziam@gmail.com>
2024-12-13 16:53:55 +0100

    terminado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,35 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
+  const [color1, setColor1] = useState("rojo");
+  const [color2, setColor2] = useState("amarillo");
+  const [color3, setColor3] = useState("verde");
 
+  function cambiarcolor1() {
+    setColor1("rojo");
+    setColor2("amarillo-opaco");
+    setColor3("verde-brillante");
+  }
+
+  function cambiarcolor2() {
+    setColor2("amarillo");
+    setColor1("rojo-brillante");
+    setColor3("verde-brillante");
+  }
+
+  function cambiarcolor3() {
+    setColor3("verde");
+    setColor1("rojo-brillante");
+    setColor2("amarillo-opaco");
+  }
+
+  return (
+    <div className="container">
+      <div className={color1} onClick={cambiarcolor1}></div>
+      <div className={color2} onClick={cambiarcolor2}></div>
+      <div className={color3} onClick={cambiarcolor3}></div>
+    </div>
+  );
+};
 export default Home;
